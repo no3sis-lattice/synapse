5583e7d56fa6fbccf73d295e140ca20c25cfc7ca
mike <h@solfunmeme.com>
2025-09-27 14:53:45 +0000

    feat(CRQ-001): Agent flakes depend on base-agent flake

--- nix/flakes/architect/flake.nix
+++ nix/flakes/architect/flake.nix
@@ -5,20 +5,17 @@
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
     flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
     pip2nix.url = "github:meta-introspector/pip2nix?ref=master";
+    base-agent.url = "path:../../base-agent"; # Reference the base agent flake
   };
 
-  outputs = { self, nixpkgs, flake-utils, pip2nix, python-env-module, ... }:
+  outputs = { self, nixpkgs, flake-utils, pip2nix, base-agent, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
 
-        pythonModule = python-env-module.lib {
-          inherit pkgs;
-          pythonPackagesFile = ../../python-packages.nix;
-        };
-        pythonEnv = pythonModule;
+        pythonEnv = base-agent.pythonEnv.${system};
       in
       {
         packages.default = pkgs.writeShellScriptBin "architect-agent" ''
