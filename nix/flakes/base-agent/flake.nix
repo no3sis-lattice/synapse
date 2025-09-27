6b6141075ed252ecbaa55e19a80a6d88a1c7ecef
mike <h@solfunmeme.com>
2025-09-27 14:52:56 +0000

    feat(CRQ-001): Create base-agent flake

---
+++ nix/flakes/base-agent/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Base agent flake providing a shared Python environment.";
+
+  inputs = {
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+    flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
+    pip2nix.url = "github:meta-introspector/pip2nix?ref=master";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pip2nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        pythonModule = import ../../modules/python-env.nix {
+          inherit pkgs;
+          pythonPackagesFile = ../../python-packages.nix;
+        };
+        pythonEnv = pythonModule;
+
+      in
+      {
+        pythonEnv = pythonEnv; # Expose the Python environment
+      }
+    );
+}
